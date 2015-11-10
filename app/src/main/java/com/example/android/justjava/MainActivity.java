2caee14278777904a61986da26b5aa4289a8b4b8
sidgvpta <siddharth.gvpta@gmail.com>
2015-11-10 19:15:49 +0000

    Pricing changes; New toasts added

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -40,7 +41,17 @@ public class MainActivity extends ActionBarActivity {
 
     private int calculatePrice() {
         int pricePerCoffee = 5;
+        int priceWhippedCream = 1;
+        int priceChocolate = 2;
+
         int totalPrice = (pricePerCoffee * quantity);
+
+        if (hasWhippedCream() == true) {
+            totalPrice = totalPrice + (priceWhippedCream * quantity);
+        }
+        if (hasChocolate() == true) {
+            totalPrice = totalPrice + (priceChocolate * quantity);
+        }
         return totalPrice;
     }
 
@@ -62,13 +73,21 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void increment(View view) {
-        quantity++;
+        if (quantity == 20) {
+            Toast.makeText(this, "Order limit reached", Toast.LENGTH_SHORT).show();
+        }
+        if (quantity < 20) {
+            quantity++;
+        }
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
         if (quantity > 0) {
             quantity--;
+        }
+        if (quantity == 0) {
+            Toast.makeText(this, "Please select at least one cup of coffee", Toast.LENGTH_SHORT).show();
         }
         displayQuantity(quantity);
     }
@@ -78,16 +97,14 @@ public class MainActivity extends ActionBarActivity {
         String ynWhippedCream;
         String ynChocolate;
 
-        CheckBox whippedCream = (CheckBox) findViewById(R.id.whippedCream);
-        if (whippedCream.isChecked() == true) {
+        if (hasWhippedCream() == true) {
             ynWhippedCream = "yes";
         }
         else {
             ynWhippedCream = "no";
         }
 
-        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate);
-        if (chocolate.isChecked() == true) {
+        if (hasChocolate() == true) {
             ynChocolate = "yes";
         }
         else {
@@ -108,6 +125,26 @@ public class MainActivity extends ActionBarActivity {
             summary += ("\nTotal: $" + price);
         }
         return summary;
+    }
+
+    private boolean hasWhippedCream() {
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whippedCream);
+        if (whippedCream.isChecked() == true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private boolean hasChocolate() {
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate);
+        if (chocolate.isChecked() == true) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
